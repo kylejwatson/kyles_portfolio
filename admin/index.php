36b794c95e2808519051f18af3bba42391bd3026
kylewatson98 <kylewatson98@gmail.com>
2016-11-06 13:39:28 +0000

    first commit

---
+++ admin/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kyle
+ * Date: 05/11/16
+ * Time: 13:08
+ */
+
+?>
---
+++ projects/project1/downloads/compr.sh
@@ -0,0 +1,1 @@
+g++ -o /home/kyle/Documents/c++-projects/boostprog /home/kyle/Documents/c++-projects/boosteg.cpp -lboost_filesystem -lboost_system; /home/kyle/Documents/c++-projects/boostprog
